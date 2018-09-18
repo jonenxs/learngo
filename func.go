be6f231515f8a26dc569da3f36b5ea8999dc34ed
聂孝爽 <xiaoshuang.nie@lanmaoly.com>
2018-09-18 15:26:25 +0800

    指针与参数传递

--- func.go
+++ func.go
@@ -7,58 +7,65 @@ import (
 	"runtime"
 )
 
-func eval(a,b int, op string) (int,error){
+func eval(a, b int, op string) (int, error) {
 	switch op {
 	case "+":
-		return a + b,nil
+		return a + b, nil
 	case "-":
-		return a - b,nil
+		return a - b, nil
 	case "*":
-		return a * b,nil
+		return a * b, nil
 	case "/":
-		q,_ := div(a,b)
-		return q,nil
+		q, _ := div(a, b)
+		return q, nil
 	default:
-		return 0,fmt.Errorf(
-			"unsupported operation: %s",op)
+		return 0, fmt.Errorf(
+			"unsupported operation: %s", op)
 	}
 }
 
-func div(a,b int) (q,r int) {
-	return a / b , a % b
+func div(a, b int) (q, r int) {
+	return a / b, a % b
 }
 
-func apply(op func(int,int) int, a,b int) int{
+func apply(op func(int, int) int, a, b int) int {
 	p := reflect.ValueOf(op).Pointer()
 	opName := runtime.FuncForPC(p).Name()
-	fmt.Printf("Calling function %s with args" + "(%d,%d)" ,opName,a,b)
-	return op(a,b)
+	fmt.Printf("Calling function %s with args"+"(%d,%d)", opName, a, b)
+	return op(a, b)
 }
 
-func pow(a,b int) int{
-	return int(math.Pow(float64(a),float64(b)))
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
 }
 
 func sum(numbers ...int) int {
 	s := 0
-	for i := range numbers{
+	for i := range numbers {
 		s += numbers[i]
 	}
 	return s
 }
 
+func swap(a, b *int) {
+	*b, *a = *a, *b
+}
+
 func main() {
-	if result,err := eval(3,4,"c"); err != nil{
+	if result, err := eval(3, 4, "c"); err != nil {
 		fmt.Println(err)
-	}else {
+	} else {
 		fmt.Println(result)
 	}
 
+	fmt.Println(eval(3, 4, "*"))
+	fmt.Println(div(13, 3))
+	fmt.Println(apply(func(a, b int) int {
+		return int(math.Pow(float64(a), float64(b)))
+	}, 3, 4))
+	fmt.Println(sum(1, 2, 3, 4, 5))
 
-	fmt.Println(eval(3,4,"*"))
-	fmt.Println(div(13,3))
-	fmt.Println(apply(func (a,b int) int{
-		return int(math.Pow(float64(a),float64(b)))
-	},3,4))
-	fmt.Println(sum(1,2,3,4,5))
+	a, b := 3, 4
+	swap(&a, &b)
+	fmt.Println(a, b)
 }
