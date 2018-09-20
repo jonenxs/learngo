79572489113a73466bb1490354da00abcbd42e82
聂孝爽 <xiaoshuang.nie@lanmaoly.com>
2018-09-20 18:44:18 +0800

    错误处理

--- errhandling/filelistingserver/filelisting/handle.go
+++ errhandling/filelistingserver/filelisting/handle.go
@@ -1,13 +1,30 @@
 package filelisting
 
 import (
+	"github.com/golang/go/src/pkg/strings"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
 
+const prefix = "/list/"
+
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+
+func (e userError) Message() string {
+	return string(e)
+}
+
 func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
-	path := request.URL.Path[len("/list/"):]
+	if strings.Index(request.URL.Path, prefix) != 0 {
+		return userError("Path must start with" + prefix)
+	}
+
+	path := request.URL.Path[len(prefix):]
 	file, error := os.Open(path)
 	if error != nil {
 		return error
--- errhandling/filelistingserver/web.go
+++ errhandling/filelistingserver/web.go
@@ -13,10 +13,27 @@ type appHandler func(writer http.ResponseWriter,
 func errWrapper(handler appHandler) func(w http.ResponseWriter, r *http.Request) {
 	return func(writer http.ResponseWriter,
 		request *http.Request) {
+
+		defer func() {
+			if r := recover(); r != nil {
+				log.Warn("Panic: %v", r)
+				http.Error(writer,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+		}()
+
 		err := handler(writer, request)
 		if err != nil {
 			log.Warn("Error handling request: %s",
 				err.Error())
+
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer,
+					userErr.Message(),
+					http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -32,8 +49,13 @@ func errWrapper(handler appHandler) func(w http.ResponseWriter, r *http.Request)
 	}
 }
 
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileList))
+	http.HandleFunc("/", errWrapper(filelisting.HandleFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
